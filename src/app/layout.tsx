3cd68b36acc10c76e8383325171ddbe43b7b5a53
AliSabah22 <aliasabah11@gmail.com>
2025-07-23 19:08:30 -0400

    changed hero picture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import Navigation from '@/components/Navigation';
 import React from 'react';
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -13,6 +14,19 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <title>Clasico Barbershop</title>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11551450864"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11551450864');
+          `}
+        </Script>
       </head>
       <body>
         <Navigation />
